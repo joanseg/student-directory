c5570b52e1d4dcf4f984eae84626d2b56b5b03f3
joansenent <joansenent@hotmail.com>
2013-12-15 10:47:01 +0000

    Array and length method 00

--- directory.rb
+++ directory.rb
@@ -1,21 +1,41 @@
-student_count = 15
-# first we print the list of students
-puts "The members list of the Atlas community"
-puts "---------------"
-puts "Charles De Barros"
-puts "Damien Hogan"
-puts "Dharmesh Raithatha"
-puts "Enrique Comba"
-puts "Gavin Cooper"
-puts "James Jenkins"
-puts "Juan Manuel"
-puts "Kurmaran"
-puts "Robert Johnson"
-puts "Ron Blanco"
-puts "Ruben Kostucki"
-puts "Sam Joseph"
-puts "Samantha Harris"
-puts "David Thompson"
-puts "Joan Senent"
+# let´s put all students into an array
+students = [
+	"Charles de Barros",
+	"Damien Hogan",
+	"Dharmesh Raithatha",
+	"Enrique Comba",
+	"Gavin Cooper",
+	"James Jenkins",
+	"Juan Manuel",
+	"Kurmaran",
+	"Robert Johnson",
+	"Ron Blanco",
+	"Ruben Kostucki",
+	"Sam Joseph",
+	"Samantha Harris",
+	"David Thompson",
+	"Joan Senent",
+]
+# and then print them
+puts "The members of the Atlas Community"
+puts "______________"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
 # finally, we print the total
-print "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students."
+
+
+
