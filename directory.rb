d332434a2558918eebac385baea7ebf1f1842367
joansenent <joansenent@hotmail.com>
2013-12-15 10:52:38 +0000

    Students.each method and do function 00

--- directory.rb
+++ directory.rb
@@ -19,21 +19,9 @@ students = [
 # and then print them
 puts "The members of the Atlas Community"
 puts "______________"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14]
+students.each do |student|
+	puts student
+end
 # finally, we print the total
 puts "Overall, we have #{students.length} great students."
 
