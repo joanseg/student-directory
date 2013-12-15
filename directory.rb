be42eb93385139165459421d28fc3ec254038cdb
joansenent <joansenent@hotmail.com>
2013-12-15 20:48:38 +0000

    adding country to the students hash 00

--- directory.rb
+++ directory.rb
@@ -1,20 +1,24 @@
 def input_students
 	puts "Please enter the name of the member"
-	puts "To finish, just hit return twice"
+	puts "To finish, just hit return three times"
 	# create an empty array
 	students = []
-	puts "students class #{students.class}"
-
 	i = 1
 	# get the first name
 	name = gets.chomp
+
+	puts "Please specify member´s country"
+	country = gets.chomp
 	# while the name is not empty, repeat thi code
 	while !name.empty? do
 		# add the student hash to the array
-		students << {:count => i, :name => name, :cohort => :december}
+		students << {:count => i, :name => name, :cohort => :december, :country => country}
 		puts "Now we have #{students.length} members"
 		# get another name from the user
+		puts "Please enter the name of the member"
 		name = gets.chomp
+		puts "Please specify member´s country"
+		country = gets.chomp
 		i += 1
 	end
 	# return the array of students
@@ -31,7 +35,7 @@ def print(students)
 	counter = 0
 	while counter <= students.count - 1
 		stu = students[counter]
-		puts "#{stu[:count]} #{stu[:name]} (#{stu[:cohort]})"
+		puts "#{stu[:count]} #{stu[:name]}, #{stu[:cohort]}, #{stu[:country]}"
 		counter += 1
 	end
 end
